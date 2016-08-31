7a4f571d40e41228b0ef34d73a612fd0e5295e79
depthlove <depthlove@163.com>
2016-08-31 13:49:45 +0800

    m08311349

--- GitLearning/GitLearning/ViewController.m
+++ GitLearning/GitLearning/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"08311320");
     NSLog(@"m08311337");
+    NSLog(@"m08311349");
 }
 
 - (void)didReceiveMemoryWarning {
